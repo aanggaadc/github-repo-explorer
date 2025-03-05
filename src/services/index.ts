f493d88bac925544b5815a0ddbcfb509a3b0cf18
anggara setiawan <aanggaadc@gmail.com>
2025-03-06 00:06:55 +0700

    add fetch repositories service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,11 @@
 import fetcher from "@/lib/fetcher";
 
-import { IApiResponse } from "@/types";
+import { IApiResponse, IRepository } from "@/types";
 
 export const fetchUsers = async (username: string) => {
   return fetcher<IApiResponse>("search/users", { q: username });
+};
+
+export const fetchRepositories = async (url: string) => {
+  return fetcher<IRepository[]>(url);
 };
